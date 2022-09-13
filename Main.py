437e6c93148dc162644d09bcc901415e678c7897
AntonyNishio <110011339+AntonyNishio@users.noreply.github.com>
2022-09-13 11:09:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,13 +6,10 @@ def selectionSort(array, size) -> List[int]:
         min_idx = step
 
         for i in range(step + 1, size):
-         
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
-            if array[i] > array[min_idx]:
+
+            if array[i] < array[min_idx]:
                 min_idx = i
          
-        # put min at the correct position
         (array[step], array[min_idx]) = (array[min_idx], array[step])
 
         
